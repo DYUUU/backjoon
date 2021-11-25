7f879fff5fff20fd9ec729a8e2c75335012f72a3
HellSkk <63494210+DYUUU@users.noreply.github.com>
2021-11-25 14:41:50 +0900

    211125

---
+++ 단계별 문제 풀기/문자열/8.다이얼.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define SWAP(a, b, tmp) (tmp=a, a=b, b=tmp)
+
+int main()
+{
+	char word[15];
+	int sum = 0;
+	scanf("%s", word);
+
+	for (int i = 0; word[i] != '\0'; i++)
+	{
+		if (word[i] >= 'A' && word[i] <= 'C')
+			sum += 3;
+		if (word[i] >= 'D' && word[i] <= 'F')
+			sum += 4;
+		if (word[i] >= 'G' && word[i] <= 'I')
+			sum += 5;
+		if (word[i] >= 'J' && word[i] <= 'L')
+			sum += 6;
+		if (word[i] >= 'M' && word[i] <= 'O')
+			sum += 7;
+		if (word[i] >= 'P' && word[i] <= 'S')
+			sum += 8;
+		if (word[i] >= 'T' && word[i] <= 'V')
+			sum += 9;
+		if (word[i] >= 'W' && word[i] <= 'Z')
+			sum += 10;
+
+	}
+
+	printf("%d", sum);
+
+
+	return 0;
+
+}
